a866ae50ee514f23a35eef66776d6688823bb21e
jlneal82 <jlneal82@gmail.com>
2016-01-22 18:03:15 -0500

    Update cachematrix.R
    _

--- cachematrix.R
+++ cachematrix.R
@@ -17,7 +17,7 @@ inv = NULL
 }
 
 
-## Write a short comment describing this function
+###Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
